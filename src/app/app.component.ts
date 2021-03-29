f3ffc550c2dd8132903cdaeb7c1be7b156b76ce2
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-03-29 09:38:24 +0200

    punto 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {Post} from './models/post.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'feisbuc-malizia';
+  postList : Array<Post> = new Array<Post>();
+
+  invia(userPost : HTMLInputElement, userName :HTMLInputElement)
+  {
+    let newPost = new Post(userName.value, userPost.value);
+    this.postList.push (newPost);
+  }
 }
